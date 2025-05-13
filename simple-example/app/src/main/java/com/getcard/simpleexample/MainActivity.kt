0aaa4e406734903db049bcb7ed6c0a1b5683bb14
Leonardo Dalberto <leonardo.dalberto@sgsistemas.com.br>
2025-05-13 11:06:30 -0300

    feat: update simple example

--- simple-example/app/src/main/java/com/getcard/simpleexample/MainActivity.kt
+++ simple-example/app/src/main/java/com/getcard/simpleexample/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.getcard.hubinterface.authentication.AuthParams
 import com.getcard.hubinterface.transaction.PaymentType
 import com.getcard.hubinterface.transaction.TransactionParams
 import com.getcard.hubinterface.transaction.TransactionResponse
@@ -16,6 +17,11 @@ import com.getcard.simpleexample.databinding.ActivityMainBinding
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        // TODO: Inserir o token de autenticação obtido no login na Hub API
+        const val AUTH_TOKEN = "SEU TOKEN"
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -32,34 +38,41 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val paymentIntent = Intent(this, PaymentActivity::class.java)
-        paymentIntent.putExtra("TRANSACTION_PARAMS",TransactionParams(
-            amount = BigDecimal("2000"),
-            paymentType = PaymentType.CREDIT
-        ))
-
         val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             val paymentAcquirer = result.data?.getStringExtra("PAYMENT_ACQUIRER")
             val response = result.data?.getParcelableExtra<TransactionResponse>("TRANSACTION_RESULT")
             if (response != null) {
                 Log.d("PaymentActivity", "Response: $response")
-                val intent = Intent(this, PaymentActivity::class.java)
-                intent.putExtra("PAYMENT_ACQUIRER", paymentAcquirer)
-                intent.putExtra("TRANSACTION_PARAMS", TransactionParams(
-                    amount = BigDecimal("2000"),
-                    nsuHost = response.nsuHost,
-                    transactionTimestamp = response.transactionTimestamp,
-                    refund = true,
-                    paymentType = PaymentType.CREDIT
-                ))
-                startActivity(intent)
+                Intent(this, PaymentActivity::class.java).also {
+                    it.putExtra("PAYMENT_ACQUIRER", paymentAcquirer)
+                    it.putExtra("TRANSACTION_PARAMS", TransactionParams(
+                        amount = BigDecimal("2000"),
+                        nsuHost = response.nsuHost,
+                        transactionTimestamp = response.transactionTimestamp,
+                        refund = true,
+                        paymentType = PaymentType.CREDIT
+                    ))
+                    it.putExtra("AUTH_PARAMS", AuthParams(AUTH_TOKEN))
+                    startActivity(it)
+                }
             }
         }
+
+        val paymentIntent = Intent(this, PaymentActivity::class.java)
+        paymentIntent.putExtra(
+            "TRANSACTION_PARAMS",
+            TransactionParams(
+                amount = BigDecimal("2000"),
+                paymentType = PaymentType.CREDIT
+            )
+        )
+        paymentIntent.putExtra("AUTH_PARAMS", AuthParams(AUTH_TOKEN))
 
         binding.transactionButton.setOnClickListener {
             startActivity(paymentIntent)
         }
 
+        // Realiza uma nova transação e estornando-a em seguida
         binding.refundButton.setOnClickListener {
             launcher.launch(paymentIntent)
         }
